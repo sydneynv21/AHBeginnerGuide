082b567e2b0a1a9a17a84680f9f88ac76dc44ba9
sydneyvachino <sydnic43@gmail.com>
2020-05-26 12:30:43 -0400

    main floor visible when user swipes to map screen, user can change floor visible by clicking buttons, user can zoom in and out of maps

--- app/src/main/java/com/example/ahbeginnerguide/MapFragment.java
+++ app/src/main/java/com/example/ahbeginnerguide/MapFragment.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
 
 /**
@@ -14,15 +19,41 @@ import android.view.ViewGroup;
  */
 public class MapFragment extends Fragment {
 
-    public MapFragment() {
-        // Required empty public constructor
-    }
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+       final SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)rootView.findViewById(R.id.imageView);
+        imageView.setImage(ImageSource.resource(R.drawable.main));
+
+        Button second = rootView.findViewById(R.id.secondButton);
+        Button main = rootView.findViewById(R.id.mainButton);
+        Button basement = rootView.findViewById(R.id.basementButton);
+        final TextView title = rootView.findViewById(R.id.mapTitle);
+
+        View.OnClickListener myListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(v.getId()==R.id.secondButton){
+                    imageView.setImage(ImageSource.resource(R.drawable.second));
+                    title.setText("Second Floor: ");
+                }
+                else if(v.getId()==R.id.mainButton){
+                    imageView.setImage(ImageSource.resource(R.drawable.main));
+                    title.setText("Main Floor: ");
+                }
+                else if(v.getId()==R.id.basementButton){
+                    imageView.setImage(ImageSource.resource(R.drawable.basement));
+                    title.setText("Basement: ");
+                }
+            }
+        };
+        second.setOnClickListener(myListener);
+        main.setOnClickListener(myListener);
+        basement.setOnClickListener(myListener);
+        return rootView;
     }
 }
